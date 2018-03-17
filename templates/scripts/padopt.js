1d3dd63cdff8038a0d5f4a9cb1d445b0a14434b8
Julien Vaubourg <julien@vaubourg.com>
2018-03-17 13:22:51 +0100

    JS reviewMode moved to a separated file and comments done

--- templates/scripts/padopt.js
+++ templates/scripts/padopt.js
@@ -1,5 +1,5 @@
-/* PadOptLiveCustomizers (PadOpt submodule)
- * Add live customization of products feature to PadOpt (ProcessWire module) 
+/* PadOpt for PadLoper
+ * Add custom client options feature to PadLoper (ProcessWire module) 
  *
  * Copyright (C) 2018 Julien Vaubourg <julien@vaubourg.com>
  * Contribute at https://github.com/jvaubourg/processwire-module-padopt
@@ -24,47 +24,7 @@ window.addEventListener('load', start, false);
  * Called once the page is loaded
  */
 function start() {
-  locateLiveCustomizers();
   reviewMode();
-}
-
-/*
- * Find viewports of live customizers and ask the input fields to call
- * changeModifier when their value is changed by the client
- */
-function locateLiveCustomizers() {
-  var livecustomizers = document.getElementsByClassName(padoptlc_classmaindiv);
-
-  for(var i = 0; i < livecustomizers.length; i++) {
-    var modifiers = livecustomizers[i].getElementsByTagName('select');
-
-    for(var j = 0; j < modifiers.length; j++) {
-      if(modifiers[j].getAttribute('name').startsWith(padoptlc_inputprefix)) {
-        modifiers[j].addEventListener('change', changeModifier, false);
-      }
-    }
-  }
-}
-
-/**
- * Called every time the value of an input field is changed inside a viewport.
- * Update the corresponding viewport by loading a different background image
- * for the layer corresponding to this option
- */
-function changeModifier() {
-
-  // Viewports are composed of as many layers as there are fields
-  // Updating an option value means updating the background image of the corresponding layer
-  var viewport_layer = document.getElementById(padoptlc_idprefix + this.name);
-
-  // Remove the prefix from the input field name, to keep only the name of
-  // the option
-  var img_name_regex = new RegExp('^' + padoptlc_inputprefix, 'i');
-  var img_name = this.name.replace(img_name_regex, '');
-
-  // The background image to show should have a name with a
-  // <option-name>_<value-chosen>.png format
-  viewport_layer.style.backgroundImage = 'url(' + padoptlc_imgurl + img_name.replace('_', '/') + '_' + this.value + '.png)';
 }
 
 /**
